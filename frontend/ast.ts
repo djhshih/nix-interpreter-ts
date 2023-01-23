305d9b7b2b4814fc1713fec7347572155a7ecb46
David J. H. Shih <djh.shih@gmail.com>
2023-01-23 22:35:35 +0800

    Changed node type to enum.

--- frontend/ast.ts
+++ frontend/ast.ts
@@ -1,81 +1,81 @@
-export type Node = 
-	  "Binding"
-	| "Function"
-  | "LetExpr"
-	| "WithExpr"
-	| "MemberExpr"
-	| "CallExpr"
-	| "BinaryExpr"
-	| "List"
-	| "Set"
-	| "Number"
-	| "Identifier"
-	;
+export enum NodeType {
+	Binding,
+	Function,
+	LetExpr,
+	WithExpr,
+	MemberExpr,
+	CallExpr,
+	BinaryExpr,
+	List,
+	Set,
+	Number,
+	Identifier
+}
 
 export interface Expr {
-	type: Node
+	type: NodeType
 }
 
 export interface Binding extends Expr {
-	type: "Binding";
+	type: NodeType.Binding;
 	identifier: string;
 	value: Expr;	
 }
 
 export interface Function extends Expr {
-	type: "Function";
+	type: NodeType.Function;
 	param: string[];
 	body: Expr;
 }
 
 export interface LetExpr extends Expr {
-	type: "LetExpr";
+	type: NodeType.LetExpr;
 	declarations: Binding[];
 	body: Expr;
 }
 
 export interface WithExpr extends Expr {
-	type: "WithExpr";
+	type: NodeType.WithExpr;
 	set: Set;
 	body: Expr;
 }
 
 export interface MemberExpr extends Expr {
-	type: "MemberExpr";
+	type: NodeType.MemberExpr;
 	set: Set;
 	name: string;
 }
 
 export interface CallExpr extends Expr {
-	type: "CallExpr";
+	type: NodeType.CallExpr;
 	arg: Expr;
 	function: Function;
 }
 
 export interface BinaryExpr extends Expr {
-	type: "BinaryExpr";
+	type: NodeType.BinaryExpr;
 	left: Expr;
 	right: Expr;
 	op: string;  // must be a binary operator
 }
 
 export interface List extends Expr {
-	type: "List";
+	type: NodeType.List;
 	elements: Expr[];
 }
 
 export interface Set extends Expr {
-	type: "Set";
+	type: NodeType.Set;
 	elements: Record<string, Expr>;	
 }
 
 export interface Number extends Expr {
-	type: "Number";
+	type: NodeType.Number;
 	value: number;
 }
 
 export interface Identifier extends Expr {
-	type: "Identifier";
+	type: NodeType.Identifier;
 	name: string;
 }
 
