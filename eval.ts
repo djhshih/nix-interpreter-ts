8e413001af1e62f4ac539c4f59e0ddde4dd9f450
David J. H. Shih <djh.shih@gmail.com>
2023-01-24 19:14:45 +0800

    Add test case.

---
+++ eval.ts
@@ -0,0 +1,17 @@
+import { readLines } from "https://deno.land/std@0.173.0/io/read_lines.ts";
+
+import Parser from "./frontend/parser.ts";
+
+run("./test/simple.nix");
+
+async function run(fpath: string) {
+	const parser = new Parser();
+
+	const file = await Deno.open(fpath);
+	for await (let line of readLines(file)) {
+		console.log(line);
+		const ast = parser.parse(line);
+		console.log(ast);
+	}
+}
+
---
+++ test/simple.nix
@@ -0,0 +1,14 @@
+1 + 2
+2 + 1
+5 + 3 * 4
+3 * 4 + 5
+2 + 4 / 2
+4 / 2 + 2
+if x > 0 then 1 else 10
+let y = 2; in y + 2
+x: x * 1
+let f = x: y: x + y; in f 2 3
+let s = { x = 1; y = 2; }; in with s; x
+{ x = 1; y = 2; }.x
+with { x = 3; z = 4; }; z
+{ x, y }: x + y
